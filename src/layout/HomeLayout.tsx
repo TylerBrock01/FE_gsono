61deb6a8a00b19b6636a7f996c93965926222607
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2025-06-30 14:26:21 -0700

    svg added

--- src/layout/HomeLayout.tsx
+++ src/layout/HomeLayout.tsx
@@ -10,7 +10,18 @@ export default function HomeLayout() {
                     <h1 className=' text-4xl antialiased italic uppercase font-semibold'>Visita guaymas</h1>
                     <div className="flex justify-between ">
                         <h2 className='italic p-1 flex-col justify-center text-2xl'>Tu guía esencial para encontrar lo mejor. </h2>
-                        <Link className='border-black border-2  p-1 rounded-full' to="/">Guaymas, Sonora, Mexico</Link>
+                        <Link className='border-black border-2  p-1 rounded-full flex gap-1' to="/">
+                            <h2>Guaymas, Sonora, Mexico</h2>
+
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                 stroke="currentColor" className="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                      d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                      d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"/>
+                            </svg>
+
+                        </Link>
                     </div>
                 </header>
                 <section className=' bg-white text-cyan-600 p-1 border-black border-b-1 flex justify-between pr-2 pl-2'>
