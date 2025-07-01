610694308182fc468fb0635f745de6d998760ad6
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2025-07-01 15:20:32 -0700

    updated position added

--- src/layout/HomeLayout.tsx
+++ src/layout/HomeLayout.tsx
@@ -8,10 +8,10 @@ export default function HomeLayout() {
             <title>Starkiller Team</title>
             <body className='bg-black min-h-screen'>
                 <header className='bg-white font-serif grid p-3 border-black border-b-1'>
-                    <h1 className=' text-4xl antialiased italic uppercase font-semibold'>Visita guaymas</h1>
-                    <div className="sm:grid md:flex md:justify-between ">
-                        <h2 className='italic p-1 flex-col justify-center text-2xl'>Tu guía esencial para encontrar lo mejor. </h2>
-                        <Link className='border-black border-2  p-1 rounded-full flex justify-center gap-1' to="/">
+                    <h1 className=' text-4xl antialiased italic uppercase font-semibold'>explora guaymas</h1>
+                    <h2 className='italic p-1 flex-col justify-center text-2xl underline'>Tu guía esencial para encontrar lo mejor. </h2>
+                    <div className="row-start-1 md:row-start-3 flex">
+                        <Link className='p-1 rounded-full flex justify-center gap-1 bg-cyan-500' to="/">
                             <h2>Guaymas, Sonora, Mexico</h2>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
                                 <path stroke-linecap="round" stroke-linejoin="round"
@@ -22,13 +22,13 @@ export default function HomeLayout() {
                         </Link>
                     </div>
                 </header>
-                <section className=' bg-white text-cyan-600 p-1 border-black border-b-1 sm:grid md:flex md:justify-between pr-2 pl-2'>
-                    <h1 className='font-serif'>Encuentra tu proxima aventura en Guaymas!</h1>
-                    <nav className='flex italic md:justify-start gap-2'>
-                        <Link className='capitalize underline' to="/">gastronomia</Link>
-                        <Link className='capitalize underline' to="/">compras</Link>
-                        <Link className='capitalize underline' to="/">experiencia</Link>
-                        <Link className='capitalize underline' to="/">servicios</Link>
+                <section className=' bg-white p-1 border-black border-b-1 sm:grid md:flex md:justify-between pr-2 pl-2'>
+                    <h1 className='font-serif text-cyan-600 flex justify-center'>Encuentra tu proxima aventura en Guaymas!</h1>
+                    <nav className='flex italic md:justify-start gap-2 text-gray-100'>
+                        <Link className='capitalize p-1 rounded-full bg-cyan-500' to="/">gastronomia</Link>
+                        <Link className='capitalize p-1 rounded-full bg-cyan-500' to="/">compras</Link>
+                        <Link className='capitalize p-1 rounded-full bg-cyan-500' to="/">experiencia</Link>
+                        <Link className='capitalize p-1 rounded-full bg-cyan-500' to="/">servicios</Link>
                     </nav>
                 </section>
                 <div>
