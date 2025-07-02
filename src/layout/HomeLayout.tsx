bda28f1a59515ed691e5ac5dd856443168f320ed
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2025-07-02 16:40:52 -0700

    icon added and bg added

--- src/layout/HomeLayout.tsx
+++ src/layout/HomeLayout.tsx
@@ -6,21 +6,25 @@ export default function HomeLayout() {
         <>
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <title>Starkiller Team</title>
+            <link rel="shortcut icon" href="/src/img/dona.jpg" type="image/x-icon"   />
             <body className='bg-black min-h-screen'>
-                <header className='bg-[url("/src/img/hero.jpg")] bg-cover bg-no-repeat md:bg-right lg:bg-center text-white font-serif grid p-3 border-black border-b-1'>
-                    <h1 className=' text-4xl antialiased italic uppercase font-semibold'>explora guaymas</h1>
-                    <h2 className='italic p-1 flex-col justify-center text-2xl underline'>Tu guía esencial para encontrar lo mejor. </h2>
-                    <div className="row-start-1 md:row-start-3 flex">
-                        <Link className='p-1 rounded-full flex justify-center gap-1 bg-cyan-500' to="/">
-                            <h2>Guaymas, Sonora, Mexico</h2>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                      d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                      d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"/>
-                            </svg>
-                        </Link>
+                <header className='z-0 bg-[url("/src/img/hero.jpg")] bg-cover bg-no-repeat md:bg-right lg:bg-center'>
+                    <div className=' font-serif grid p-3 border-black border-b-1 z-1 bg-black/70 text-white'>
+                        <h1 className=' text-4xl  italic capitalize font-semibold'>explora guaymas</h1>
+                        <h2 className='italic p-1 flex-col justify-center text-2xl underline'>Tu guía esencial para encontrar lo mejor. </h2>
+                        <div className="row-start-1 md:row-start-3 flex">
+                            <Link className='p-1 rounded-full flex justify-center gap-1 bg-cyan-500' to="/">
+                                <h2>Guaymas, Sonora, Mexico</h2>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                          d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                          d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"/>
+                                </svg>
+                            </Link>
+                        </div>
                     </div>
+
                 </header>
                 <section className=' bg-white p-1 border-black border-b-1 sm:grid md:flex md:justify-between pr-2 pl-2'>
                     <h1 className='font-serif text-cyan-600 flex justify-center items-center'>Encuentra tu proxima aventura en Guaymas!</h1>
