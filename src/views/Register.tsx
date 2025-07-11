0bae3c54cb927af8657659a462c7febb786fdcc2
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2025-07-10 18:24:39 -0700

    basic bussines input information updated

--- src/views/Register.tsx
+++ src/views/Register.tsx
@@ -3,7 +3,7 @@ export default function Register() {
         <>
             <h1 className='text-3xl text-white capitalize'>registrar nuevo negocio</h1>
             <div className='bg-cyan-600 text-white flex flex-col items-center p-2 '>
-                <form className='p-1 border-black border-2'>
+                <form className='p-1 border-black border-2 capitalize bg-cyan-800'>
                     <h2 className='text-xl'>nombre del negocio</h2>
                     <input className='bg-gray-400 rounded-full' type='text' placeholder='negocio' disabled={true}/>
                     <h2 className='text-xl'>logo</h2>
@@ -17,7 +17,7 @@ export default function Register() {
                     <h2 className='text-xl'>descripcion</h2>
                     <input className='bg-gray-400 rounded-full' type='text' placeholder='descripcion'disabled={true}/>
                     <h2 className='text-xl'>precio</h2>
-
+                    <h2 className='text-xl'>cadena</h2>
                 </form>
             </div>
         </>
