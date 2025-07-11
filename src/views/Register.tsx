331338b77a30bdf7db5e6a970f82450df1a759bd
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2025-07-10 23:14:13 -0700

    test bugg input

--- src/views/Register.tsx
+++ src/views/Register.tsx
@@ -5,17 +5,17 @@ export default function Register() {
             <div className='bg-cyan-600 text-white flex flex-col items-center p-2 '>
                 <form className='p-1 border-black border-2 capitalize bg-cyan-800'>
                     <h2 className='text-xl'>nombre del negocio</h2>
-                    <input className='bg-gray-400 rounded-full' type='text' placeholder='negocio' disabled={true}/>
-                    <h2 className='text-xl'>logo</h2>
-                    <input className='bg-gray-400 rounded-full' type='text' placeholder='logo' disabled={true}/>
-                    <h2 className='text-xl'>direccion</h2>
-                    <input className='bg-gray-400 rounded-full' type='text' placeholder='direccion'disabled={true}/>
-                    <h2 className='text-xl'>facebook</h2>
-                    <input className='bg-gray-400 rounded-full' type='text' placeholder='facebook'disabled={true}/>
-                    <h2 className='text-xl'>instagram</h2>
-                    <input className='bg-gray-400 rounded-full' type='text' placeholder='instagram'disabled={true}/>
-                    <h2 className='text-xl'>descripcion</h2>
-                    <input className='bg-gray-400 rounded-full' type='text' placeholder='descripcion'disabled={true}/>
+                    <input className='bg-gray-400 rounded-full' type='text' placeholder='negociuu'/>
+                    {/*<h2 className='text-xl'>logo</h2>*/}
+                    {/*<input className='bg-gray-400 rounded-full' type='text' placeholder='logo' disabled={true}/>*/}
+                    {/*<h2 className='text-xl'>direccion</h2>*/}
+                    {/*<input className='bg-gray-400 rounded-full' type='text' placeholder='direccion'disabled={true}/>*/}
+                    {/*<h2 className='text-xl'>facebook</h2>*/}
+                    {/*<input className='bg-gray-400 rounded-full' type='text' placeholder='facebook'disabled={true}/>*/}
+                    {/*<h2 className='text-xl'>instagram</h2>*/}
+                    {/*<input className='bg-gray-400 rounded-full' type='text' placeholder='instagram'disabled={true}/>*/}
+                    {/*<h2 className='text-xl'>descripcion</h2>*/}
+                    {/*<input className='bg-gray-400 rounded-full' type='text' placeholder='descripcion'disabled={true}/>*/}
                     <h2 className='text-xl'>precio</h2>
                     <h2 className='text-xl'>cadena</h2>
                 </form>
