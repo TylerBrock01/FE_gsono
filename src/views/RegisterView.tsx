80c35531482f2245e8f755d7451a8240e0ce0aad
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2025-07-11 23:26:52 -0700

    input added & react-hook-form installed

--- src/views/RegisterView.tsx
+++ src/views/RegisterView.tsx
@@ -1,17 +1,24 @@
+import {useForm} from 'react-hook-form'
+
 export default function RegisterView() {
+    const {register, handleSubmit, watch, formState: {errors}} = useForm();
+    const handleRegister = () => {
+        console.log('desde handle')
+    }
     return(
         <>
             <form
-                onSubmit={() => {}}
+                onSubmit={handleSubmit(handleRegister)}
                 className="bg-white px-5 py-20 rounded-lg space-y-10 mt-10"
             >
                 <div className="grid grid-cols-1 space-y-3">
-                    <label htmlFor="name" className="text-2xl text-slate-500">Nombre</label>
+                    <label htmlFor="name" className="text-2xl text-slate-500 capitalize">nombre de negocio</label>
                     <input
                         id="name"
                         type="text"
-                        placeholder="Tu Nombre"
+                        placeholder="Negocio"
                         className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+                        {...register('name', {required: 'nombre es requerido'})}
                     />
                 </div>
 
