e1d898fac4807de1973b078fe677d537eafe31f5
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2025-07-14 20:22:47 -0700

    style added

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -1,5 +1,5 @@
 export default function ErrorMessage ({children} : {children: React.ReactNode}) {
     return(
-        <p className='text-red-600 capitalize'>{children}</p>
+        <p className='text-red-600 bg-red-50 p-2 font-bold text-sm text-center capitalize'>{children}</p>
     )
 }
