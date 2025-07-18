7decbffabb46fbe82b5baa3194960e303ba19d4e
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2025-07-17 22:50:46 -0700

    default added fixed

--- src/components/HomeComponent/NavBarComponent.tsx
+++ src/components/HomeComponent/NavBarComponent.tsx
@@ -4,7 +4,7 @@ import {Link} from "react-router-dom";
 import {scrollToTop} from "../MiscComponent/ScrollTop.ts";
 
 
-export function NavBarComponent() {
+export default function NavBarComponent() {
 
 
     // const categories = [
