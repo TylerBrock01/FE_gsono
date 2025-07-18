1e6fe29dca590f94f2bfa0ada14fbf83d1367a98
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2025-07-17 22:44:25 -0700

    scroll top

--- src/components/HomeComponent/NavBarComponent.tsx
+++ src/components/HomeComponent/NavBarComponent.tsx
@@ -1,9 +1,16 @@
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 import { Bars3Icon } from '@heroicons/react/24/solid'
 import {Link} from "react-router-dom";
-
+const scrollToTop = () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+};
 
 export default function NavBarComponent() {
+
+
     // const categories = [
     //     {name: 'gastronomia', icon: 'food-drink', id: 1, path:"/"},
     //     {name: 'compras', icon: 'shopping-bag', id:2, path:"/"},
@@ -16,16 +23,17 @@ export default function NavBarComponent() {
     return(
         <nav className='flex ' >
             <Menu>
-                <MenuButton className="capitalize focus:outline-0   gap-2 rounded-md bg-cyan-700  border-1 border-cyan-800 hover:border-cyan-400 px-3 py-1 text-sm/6 font-semibold text-white">
+                <MenuButton onClick={scrollToTop} className="capitalize focus:outline-0  gap-2 rounded-md bg-cyan-700  border-1 border-cyan-800 hover:border-cyan-400 px-3 py-1 text-sm/6 font-semibold text-white">
                     <Bars3Icon className='h-6 w-6' aria-hidden='true'/>
                 </MenuButton>
 
                 <MenuItems
+
                     anchor="bottom end"
                     className="animate-slide-in-right grid gap-1 z-5 mt-2 focus:outline-0 capitalize  rounded-xl bg-cyan-800   p-1 text-sm/6 text-white  "
                 >
                     <MenuItem>
-                        <Link to='#' className="bg-cyan-500 group flex  items-center gap-2 rounded-lg px-3 py-1 border-1 border-cyan-800 hover:border-cyan-400 hover:bg-cyan-600">
+                        <Link to='/register' className="bg-cyan-500 group flex  items-center gap-2 rounded-lg px-3 py-1 border-1 border-cyan-800 hover:border-cyan-400 hover:bg-cyan-600">
                             gastronomic
                         </Link>
                     </MenuItem>
