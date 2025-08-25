0f3b71010d7bff7c266f704ddb50054017db8fcc
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2025-08-25 15:24:20 -0700

    log in demo 1

---
+++ src/ButtonStyle.tsx
@@ -0,0 +1,7 @@
+export default function ButtonStyle(name: string) {
+    return(
+        <>
+            <button className="bg-blue-500 rounded-md text-white capitalize p-2">{name}</button>
+        </>
+    )
+}
--- src/views/LogIn.tsx
+++ src/views/LogIn.tsx
@@ -1,16 +1,28 @@
+import ButtonStyle from "../ButtonStyle.tsx";
+
 export default function LogIn() {
     const xd= 'w-80'
     return(
         <>
-            <div className='flex justify-center p-3 '>
-                <div className='bg-gray-300 p-2 rounded-md'>
+            <div className=' flex justify-center p-3 '>
+                <div className='bg-gray-100/80 p-2 rounded-md'>
                     <div>
                         <img alt='xd' className={xd} src="https://apps9.itson.edu.mx/CIA/img/ITSON-MARCA.png"/>
                         <img alt='xd' className={xd} src="https://apps9.itson.edu.mx/CIA/img/CIA.png"/>
                     </div>
-                    <div className='flex justify-center'>
-                        <input className='bg-white rounded-md px-2 text-slate-700/70' type="text" placeholder="Ingresar ID ITSON"/>
-                        <input className='' type={"submit"}/>
+                    <div className='flex flex-col gap-3 justify-center p-10'>
+                        <label className='grid'>
+                            <input className=' focus:outline-blue-400 bg-white rounded-md px-2 text-slate-700/70' type="text" placeholder="Ingresar ID ITSON"/>
+                        </label>
+                        <label className='grid'>
+                            <input className=' focus:outline-blue-400 bg-white rounded-md px-2 text-slate-700/70' type="password" placeholder="Ingresar clave"/>
+                        </label>
+                        <label className='grid'>
+                            { ButtonStyle('ingresar')}
+                        </label>
+                        <label className='flex justify-center'>
+                            <a className="text-blue-500 capitalize" href="/">aviso de privacidad</a>
+                        </label>
                     </div>
 
                 </div>
