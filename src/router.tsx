639686f06f043b74b4bafc0e39e75dc81ea993a9
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2025-08-26 12:39:21 -0700

    home profile added

--- src/router.tsx
+++ src/router.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route} from  'react-router-dom'
 import LogIn from "./views/LogIn.tsx";
 import HomeLayout from "./layout/HomeLayout.tsx";
 import TestView from "./views/TestView.tsx";
+import ProfileLayout from "./layout/ProfileLayout.tsx";
+import HomeView from "./views/HomeView.tsx";
 
 export default function  Router(){
     return(
@@ -9,7 +11,12 @@ export default function  Router(){
             <Routes>
                 <Route element={<HomeLayout />}>
                     <Route path="/" element={<LogIn />} />
-                    <Route path='/test' element={<TestView/>} />
+                    <Route path="/test" element={<TestView/>} />
+                </Route>
+            </Routes>
+            <Routes>
+                <Route element={<ProfileLayout />}>
+                    <Route path='/home' element={<HomeView/>} />
                 </Route>
             </Routes>
         </BrowserRouter>
