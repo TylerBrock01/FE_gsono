436cf34d89e1ac5080669d8789f19e5b842364d1
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2025-08-26 08:46:52 -0700

    padding added mobile

--- src/layout/HomeLayout.tsx
+++ src/layout/HomeLayout.tsx
@@ -4,7 +4,7 @@ export default function HomeLayout() {
     return(
         <>
             {/*<h1 className='bg-sky-500'>Home Log in</h1>*/}
-            <div className="bg-sky-500">
+            <div className="bg-sky-500 py-30">
                 <Outlet/>
             </div>
         </>
