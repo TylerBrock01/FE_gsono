27e177b279aea1c1827064b8ed20d8ad4233f7bc
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2025-08-28 10:22:42 -0700

    navbar updated

--- src/layout/ProfileLayout.tsx
+++ src/layout/ProfileLayout.tsx
@@ -4,11 +4,13 @@ import NavBarSm from "../views/component/NavBarSm.tsx";
 export default function profileLayout() {
     return(
         <>
-            <label className="grid bg-sky-500">
-                <img alt='xd' className=' h-15 ' src="https://apps9.itson.edu.mx/CIA/img/ITSON-MARCA.png"/>
-            </label>
-            <div className="bg-sky-500 p-1 flex gap-1 justify-start  capitalize">
-                {NavBarSm()}
+            <div className="md:grid grid-cols-2">
+                <label className="grid bg-sky-500">
+                    <img alt='xd' className=' h-15 ' src="https://apps9.itson.edu.mx/CIA/img/ITSON-MARCA.png"/>
+                </label>
+                <div className="bg-sky-500 p-1 flex gap-1 justify-start md:justify-end  capitalize">
+                    {NavBarSm()}
+                </div>
             </div>
             <div className="bg-sky-200 p-2">
                 <Outlet/>
